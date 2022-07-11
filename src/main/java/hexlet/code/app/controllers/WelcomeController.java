96dc2c4f0e3a36ebbf4fbf4f794d3e78a5e42e83
VYATCHESLAV BALAKHONOV <slaviklal@yandex.ru>
2022-07-11 13:35:09 +0300

    trying to connect to Rollbar#2

--- src/main/java/hexlet/code/app/controllers/WelcomeController.java
+++ src/main/java/hexlet/code/app/controllers/WelcomeController.java
@@ -1,13 +1,24 @@
 package hexlet.code.app.controllers;
 
+import com.rollbar.notifier.Rollbar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Autowired
+    Rollbar rollbar;
+
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
     @GetMapping("/welcome")
     public String rootWelcome() {
-        return "Welcome to Spring";
+
+        rollbar.debug("Here is some debug message");
+        return rollbarToken;
     }
 }
